935e2b75fd61ef44e2c776cfd2c09c1d7beede56
hanxingzhi <1140039802@qq.com>
2018-08-01 18:30:16 +0800

    add leetcode LongestCommonSubString

--- LeetCode/LongestPalindromicSubstring.py
+++ LeetCode/LongestPalindromicSubstring.py
@@ -1,40 +1,30 @@
-
+import unittest
 """
 giving a string s, find the longest palindromic substring of s
 """
 
-def findLongestPalindromicAtMid(s, left, mid, right):
-    '''
+class Solution:
+    def findLongestPalidromic(self, s):
+        current = ''
 
-    :return:
-    '''
+        for index in range(0, len(s) - 1):
+            s[index]
 
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
 
+    def test_findLongestPalidromic_common(self):
+        s = 'aba'
+        r_len, r_str = self.solution.findLongestPalidromic(s)
+        self.assertEqual(len(s), r_len)
+        self.assertEqual(s, r_str)
 
-def findLongestPalindromic(s, left, right):
-    '''
-
-    :return:
-    '''
-    if right == left:
-        return 0, s[left]
-    if right - left == 1 and s[left] == s[right]:
-        return len(s[left:right]), s[left:right]
-    mid = (left + right) // 2
-    left_len, left_str = findLongestPalindromic(s, left, mid)
-    mid_len, mid_str = findLongestPalindromicAtMid(s, left, mid, right)
-    right_len, right_str = findLongestPalindromic(s, mid + 1, right)
-
-    if left_len > mid_len and left_len > right_len:
-        return left_len, left_str
-    elif mid_len > left_len and mid_len > right_len:
-        return mid_len, mid_str
-    else:
-        return right_len, right_str
-
-def test_findLongestPalindromic():
-    s = 'abba'
-    print(findLongestPalindromic(s, 0, 3))
+    def test_findLongestPalidromic_in(self):
+        s = 'caba'
+        r_len, r_str = self.solution.findLongestPalidromic(s)
+        self.assertEqual(3, r_len)
+        self.assertEqual('aba', r_str)
 
 if __name__ == "__main__":
-    test_findLongestPalindromic()
+    unittest.main()

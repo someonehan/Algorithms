1294ddae45e27d4973d7d21c6a9d7f388fe9b7be
han <goodboyhan@hotmail.com>
2018-10-25 15:38:37 +0800

    LeetCode TwoSum

--- LeetCode/Array/ThreeSum.py
+++ LeetCode/Array/ThreeSum.py
@@ -9,17 +9,25 @@ from ThreeSumClosest import ShortInputException
 Given an array nums of n integers, are there elements a, b, c in nums, such that a + b + c = 0?
 Find all unique triplets in the array which gives the sum of zero
 """
-
+"""
+if I want get a + b + c = 0
+1. should sort the array
+2. 
+"""
 
 class Solution:
     def threeSum(self, nums):
         if len(nums) < 3:
             raise ShortInputException(len(nums), 3)
-        start, end = 0, len(nums)
+
+        nums.sort()
+        start, end = 0, len(nums) - 1
         target_sum = 0
         for i in range(len(nums)):
             target_sum = nums[i] * -1;
+            start = i + 1
 
+            nums[start] + end
 
 class TestSolution(unittest.TestCase):
     def setUp(self):

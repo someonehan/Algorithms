01109d9cb941eb0704bce62b26bf0a5f7ec6bdec
hanxingzhi <1140039802@qq.com>
2018-10-28 21:16:38 +0800

    LeetCode AddTwoNumbers

---
+++ LeetCode/String/LongestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,19 @@
+import unittest
+
+"""
+Given a string, find the longest substring without repeating characters
+"""
+
+
+class Solution:
+    def longSubstring(self, s):
+        pass
+
+
+class TestSolution(unittest.TestCase):
+    def test_long_substring(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()

2493cfe1c0fb8dafabb09fd9466b4b6050b0bf4d
hanxingzhi <1140039802@qq.com>
2018-11-25 17:56:46 +0800

    LeetCode:generateParentheses 01

---
+++ LeetCode/String/GenerateParentheses.py
@@ -0,0 +1,63 @@
+import unittest
+
+"""
+given n pairs of parentheses, writen a function to generate all combinations of well-formed parentheses
+for example n = 3
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+"""
+class Solution:
+    def generateParentheses(self, n):
+        def generate(A=[]):
+            if len(A) == n*2:
+                if valid(A):
+                    ans.append(''.join(A))
+            else:
+                A.append('(')
+                generate(A)
+                A.pop()
+                A.append(')')
+                generate(A)
+                A.pop()
+
+        def valid(A):
+            """
+            check whether a sequence is valid,the net number of opening brackets minus closing
+            brackets. if it falls below zero at any time or doesn't end in zero, the sequence is
+            invalid--otherwise it is valid
+            """
+            balance = 0
+            for c in A:
+                if c == '(':
+                    balance += 1  # matching opening brackets
+                else:
+                    balance -= 1  # matching closing brackets
+                if balance < 0:
+                    return False  # every time it falls below zero
+            return balance == 0   # end in zero
+
+        ans = []
+        generate()
+        return ans
+
+    def generateParentheses2(self, n):
+        pass
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_three_generateParentheses(self):
+        result = self.solution.generateParentheses(3)
+        self.assertTrue(5, len(result))
+
+    def test_one_generateParentheses(self):
+        result = self.solution.generateParentheses(1)
+        self.assertTrue(1, len(result))
+
+if __name__ == "__main__":
+    unittest.main()
